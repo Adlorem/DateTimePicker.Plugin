fc42ba4950edf8980efa1befc9d601f885ef013c
Adam Biderman <adam.biderman@artisso.pl>
2021-01-23 13:35:00 +0100

    removed unused dependencies

--- DateTimePicker.Plugin.Sample/SampleApp.iOS/AppDelegate.cs
+++ DateTimePicker.Plugin.Sample/SampleApp.iOS/AppDelegate.cs
@@ -22,8 +22,6 @@ namespace SampleApp.iOS
         //
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
-            Rg.Plugins.Popup.Popup.Init();
-
             global::Xamarin.Forms.Forms.Init();
             LoadApplication(new App());
 
